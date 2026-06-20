01277c9c26f57e0a0cb8bfb51eeb30dfaa213e4a
Hira807 <ht0519839@gmail.com>
2026-06-20 12:56:26 +0500

    app.py

--- app.py
+++ app.py
@@ -101,5 +101,3 @@ def health():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-
-Thats the code I generated to repalce it with app.py code
